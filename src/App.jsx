a43b6e48c2f28b5c1912efd1e2ec4c6b15f86ad6
Meshack Yaro <yaromeshack@gmail.com>
2025-07-21 23:33:50 +0100

    message greeter deployed

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import abi from "./api.json";
+import { ethers } from "ethers";
+
+const contractAddress = "0x3F2f8D53F9A306eF2A1eF819EC8844883c1916a5";
+
+function App() {
+  const [text, setText] = useState("");
+  const [message, setMessage] = useState("");
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  const handleSet = async () => {
+    try {
+      if (!text) {
+        alert("Please enter a message before setting.");
+        return;
+      }
+
+      if (window.ethereum) {
+        await requestAccount();
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+
+        const tx = await contract.setMessage(text); 
+        const txReceipt = await tx.wait();
+        console.log("Transaction successful:", txReceipt);
+      } else {
+        console.error("MetaMask not found. Please install MetaMask to use this application.");
+      }
+    } catch (error) {
+      console.error("Error setting message:", error);
+    }
+  };
+
+  const handleGet = async () => {
+    
+      if (window.ethereum) {
+        await requestAccount();
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+
+        const tx = await contract.getMessage();
+        setMessage(tx);
+      } else {
+        console.error("MetaMask not found. Please install MetaMask to use this application.");
+      }
+   
+  };
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Set Message on Smart Contract</h1>
+      <input
+        type="text"
+        placeholder="type your message"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleSet}>Set Message</button>
+      <button onClick={handleGet}>Get Message</button>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
